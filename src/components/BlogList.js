77df07427868de3c6b9233f7b3f89799d569fff6
Markus Lampola <lampola@hotmail.com>
2018-11-26 22:05:42 +0200

    5.8 blogilistan frontend, osa 8

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Blog from './Blog'
 
-const BlogList = ({blogs, likeHandler}) => {
+const BlogList = ({ blogs, likeHandler }) => {
   return (
     <div>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} likeHandler={likeHandler}/>
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} likeHandler={likeHandler} />
+        )
+      }
     </div>)
 }
 
