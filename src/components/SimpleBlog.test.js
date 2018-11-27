56c7ac2ff9b5635d002f3114c29c5a8f2fa58015
Markus Lampola <lampola@hotmail.com>
2018-11-27 21:31:03 +0200

    5.13 blogilistan testit, osa 2

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -19,4 +19,28 @@ describe.only('<SimpleBlog />', () => {
     expect(titleDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(`blog has ${blog.likes} likes`)
   })
+
+  it('clicking the button twice calls event handler twice', () => {
+    const blog = {
+      title: 'Tilintarkastus eilen, tänään ja huomenna',
+      author: 'P.K.',
+      url: 'http://google.fi',
+      likes: 0
+    }
+  
+    const mockHandler = jest.fn()
+  
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+  
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+  
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
